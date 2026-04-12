36cd04c76a81873e6518c8aedafb3c5f4df6067b
Kiana H <khoss010@ucr.edu>
2026-04-11 23:55:28 -0700

    I fixed up the buttons and their formatting!

--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -1,15 +1,22 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-nsbe-yellow-100 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-nsbe-gray-100 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="bg-nsbe-yellow-100 flex w-2/3 flex-col items-center gap-6 justify-self-center rounded-3xl p-8">
+      <div className="bg-nsbe-gray-900 w-1/2 p-5 text-center text-3xl text-white">
+        {props.text1}
+      </div>
 
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-nsbe-red-100 w-full p-5 text-left">
+      <div className="flex w-1/2 flex-row gap-4">
+        <div className="bg-nsbe-gray-100 w-full rounded-full p-5 text-center text-3xl">
           {props.text2}
+        </div>
+        <div className="bg-nsbe-green-100 w-full rounded-full p-5 text-center text-3xl">
+          Click
         </div>
       </div>
 
-      <div className="bg-nsbe-green-100 w-1/2 p-5">{props.text3}</div>
+      <div className="bg-nsbe-red-100 w-1/2 p-5 text-center text-3xl">
+        {props.text3}
+      </div>
     </div>
   );
 };
