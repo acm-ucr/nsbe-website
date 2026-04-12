d32bc0e513cecc8d15f528c4faa1c652bc739aa3
xelqo <atsai345@gmail.com>
2026-04-11 19:17:04 -0700

    Alex Practice Issue

--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -1,15 +1,22 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-nsbe-yellow-100 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-nsbe-gray-100 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="bg-nsbe-yellow-100 mx-auto flex w-5/6 flex-col items-center gap-4 rounded-2xl p-8">
+      <div className="bg-nsbe-gray-50 m-3 w-full rounded-sm p-5 text-center text-amber-50">
+        {props.text1}
+      </div>
 
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-nsbe-red-100 w-full p-5 text-left">
+      <div className="flex w-full flex-row justify-center gap-1">
+        <div className="bg-nsbe-gray-100 m-0 w-1/2 rounded-full p-5 text-center">
+          {props.text2}
+        </div>
+        <div className="bg-nsbe-green-100 m-0 w-1/2 rounded-full p-5 text-center">
           {props.text2}
         </div>
       </div>
 
-      <div className="bg-nsbe-green-100 w-1/2 p-5">{props.text3}</div>
+      <div className="bg-nsbe-red-100 m-5 w-full rounded-sm p-5 text-center">
+        {props.text3}
+      </div>
     </div>
   );
 };
