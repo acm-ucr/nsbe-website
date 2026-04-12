941f377cb209c22bb7c5f9db4ccbb8490cb06965
Kiana H <khoss010@ucr.edu>
2026-04-12 00:30:32 -0700

    I fixed formatting issues

--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -2,8 +2,8 @@ const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
     <div className="bg-nsbe-yellow-100 flex w-2/3 flex-col items-center gap-6 justify-self-center rounded-3xl p-8">
       <div className="bg-nsbe-gray-50 w-1/2 p-6 text-center text-3xl text-white">
-  {props.text1}
-</div>
+        {props.text1}
+      </div>
 
       <div className="flex w-1/2 flex-row gap-4">
         <div className="bg-nsbe-gray-100 w-full rounded-full p-5 text-center text-3xl">
