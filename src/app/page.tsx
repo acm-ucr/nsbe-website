61c87551ae86259afec6136798106b6a1fd98611
AYYYYYYJ <ajedwards881@gmail.com>
2026-04-14 16:55:00 -0700

    Finished FlexBoxLesson Issue #3: formatted boxes correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Example from "@/components/practice";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="Change Me!" text2="Click" text3="Props" />
+      <Example text1="Hello" text2="Click" text3="Props" />
     </div>
   );
 };
--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -1,17 +1,59 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-nsbe-yellow-100 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-nsbe-gray-100 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="bg-nsbe-yellow-100 flex w-2/3 flex-col items-center gap-6 justify-self-center rounded-lg p-8">
+      <div className="bg-nsbe-gray-50 w-1/2 p-5 text-center text-2xl text-white">
+        {props.text1}
+      </div>
 
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-nsbe-red-100 w-full p-5 text-left">
+        <div className="bg-nsbe-gray-100 w-full rounded-full p-5 text-center text-2xl">
+          {props.text2}
+        </div>
+        <div className="bg-nsbe-green-100 w-full rounded-full p-5 text-center text-2xl">
           {props.text2}
         </div>
       </div>
 
-      <div className="bg-nsbe-green-100 w-1/2 p-5">{props.text3}</div>
+      <div className="bg-nsbe-red-100 w-1/2 p-5 text-center text-2xl">
+        {props.text3}
+      </div>
     </div>
   );
 };
 
 export default Example;
+
+// const Example = (props: {
+//   text1: string;
+//   text2: string;
+//   text3: string;
+//   text4: string;
+// }) => {
+//   return (
+//     <div className="bg-nsbe-yellow-100 flex flex-col w-2/3 items-center justify-center gap-6 p-10 rounded-xl mx-auto">
+
+//       {/* Top box */}
+//       <div className="bg-nsbe-gray-100 w-1/2 p-5 text-center text-white text-xl rounded-md">
+//         {props.text1}
+//       </div>
+
+//       {/* Middle row (2 buttons) */}
+//       <div className="flex flex-row gap-6">
+//         <div className="bg-nsbe-gray-100 px-10 py-4 text-center rounded-full text-lg">
+//           {props.text2}
+//         </div>
+//         <div className="bg-nsbe-green-100 px-10 py-4 text-center rounded-full text-lg">
+//           {props.text3}
+//         </div>
+//       </div>
+
+//       {/* Bottom box */}
+//       <div className="bg-nsbe-red-100 w-1/2 p-5 text-center text-lg rounded-md">
+//         {props.text4}
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default Example;
