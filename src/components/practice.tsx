6b9adcc1b4f67b37fa3a04b1b6a0f844829066d0
AYYYYYYJ <ajedwards881@gmail.com>
2026-04-15 09:42:39 -0700

    Removed extra comments

--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -22,38 +22,3 @@ const Example = (props: { text1: string; text2: string; text3: string }) => {
 };
 
 export default Example;
-
-// const Example = (props: {
-//   text1: string;
-//   text2: string;
-//   text3: string;
-//   text4: string;
-// }) => {
-//   return (
-//     <div className="bg-nsbe-yellow-100 flex flex-col w-2/3 items-center justify-center gap-6 p-10 rounded-xl mx-auto">
-
-//       {/* Top box */}
-//       <div className="bg-nsbe-gray-100 w-1/2 p-5 text-center text-white text-xl rounded-md">
-//         {props.text1}
-//       </div>
-
-//       {/* Middle row (2 buttons) */}
-//       <div className="flex flex-row gap-6">
-//         <div className="bg-nsbe-gray-100 px-10 py-4 text-center rounded-full text-lg">
-//           {props.text2}
-//         </div>
-//         <div className="bg-nsbe-green-100 px-10 py-4 text-center rounded-full text-lg">
-//           {props.text3}
-//         </div>
-//       </div>
-
-//       {/* Bottom box */}
-//       <div className="bg-nsbe-red-100 w-1/2 p-5 text-center text-lg rounded-md">
-//         {props.text4}
-//       </div>
-
-//     </div>
-//   );
-// };
-
-// export default Example;
