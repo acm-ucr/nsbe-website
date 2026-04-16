887946c92267779229afcba4fcc10d7a47e2b396
Mostafa Elmallah <mmmelmallah@gmail.com>
2026-04-15 17:27:42 -0700

    did first issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Example from "@/components/practice";
+import Button from "@/components/Button";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Example text1="Change Me!" text2="Click" text3="Props" />
+      <Button />
     </div>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,9 @@
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <button className="rounded-lg border-2 border-yellow-400 px-6 py-3 font-semibold tracking-wide text-yellow-400 uppercase">
+      LEARN MORE
+    </button>
+  );
 };
 
 export default Button;
