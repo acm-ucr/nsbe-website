631b96878b0a2fc66453d2507cae7d1d17c00260
Alaya Billinger <Alaya.sanai@icloud.com>
2026-04-16 17:31:34 -0700

    Made the navbar with links to other pages

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,8 +1,6 @@
-
-
 export const items = [
-  {name: "HOME", link: "/home"},
-  {name: "ABOUT", link: "/about"},
-  {name: "BOARD", link: "/board"},
-  {name: "EVENTS", link: "/events"},
+  { name: "HOME", link: "/home" },
+  { name: "ABOUT", link: "/about" },
+  { name: "BOARD", link: "/board" },
+  { name: "EVENTS", link: "/events" },
 ];
