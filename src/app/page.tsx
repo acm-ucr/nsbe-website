405b6436c5fdf945ff2f638a5d2d5fc8dc00262b
Mostafa Elmallah <mmmelmallah@gmail.com>
2026-04-20 12:59:16 -0700

    took out the button compononent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Example from "@/components/practice";
-import Button from "@/components/Button";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Example text1="Change Me!" text2="Click" text3="Props" />
-      <Button />
     </div>
   );
 };
