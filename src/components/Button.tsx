c6de264efab14dfb25acd1f2c50e28910708a6de
Mostafa Elmallah <mmmelmallah@gmail.com>
2026-04-21 15:09:38 -0700

    made it not hardcoded

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,11 @@
 interface ButtonProps {
-  text : string;
+  text: string;
 }
 
 const Button = ({ text }: ButtonProps) => {
   return (
     <button className="rounded-lg border-2 border-yellow-400 px-6 py-3 font-semibold tracking-wide text-yellow-400 uppercase">
-     <div> {text} </div>
+      <div> {text} </div>
     </button>
   );
 };
