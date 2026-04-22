a6197793d8cfe961dd976ffd67478b38b698bfbc
Alaya Billinger <Alaya.sanai@icloud.com>
2026-04-22 15:53:10 -0700

    made fixed based on comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 import NSBElogo from "@/public/Logo.webp";
 import Image from "next/image";
 import Link from "next/link";
+import navigations from "@/data/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -10,40 +11,34 @@ const Navbar = () => {
   const getLinkStyle = (path: string) => {
     const isActive = pathname === path;
 
-    return `mt-5 px-10 py-2 rounded-md text-nsbe-yellow-100 transition-colors ${
+    return `mt-5 px-8 py-2 rounded-md text-nsbe-yellow-100 transition-colors ${
       isActive
-        ? "bg-nsbe-yellow-100/50 border-3"
-        : "hover:bg-nsbe-yellow-100/20"
+        ? "bg-nsbe-yellow-100/20 border-3"
+        : "hover:bg-nsbe-yellow-100/20 hover:border-3 "
     }`;
   };
   return (
     <div className="flex flex-col">
-      <div className="bg-nsbe-yellow-100/5 flex h-20 w-full flex-row">
-        <Image src={NSBElogo} alt="NSBE Logo" className="size-20"></Image>
-        <div className="flex flex-col">
-          <div className="text-nsbe-yellow-100 pt-5 text-lg font-light">
-            NSBE AT UCR
+      <div className="bg-nsbe-gray-50 border-nsbe-yellow-100 flex h-20 w-full flex-row border-b-8">
+        <Image src={NSBElogo} alt="NSBE Logo" width={60} height={15} />
+        <div className="flex w-full justify-between">
+          <div className="flex flex-col">
+            <div className="text-nsbe-yellow-100 pt-5 text-lg font-light">
+              NSBE AT UCR
+            </div>
+            <div className="text-nsbe-yellow-100 text-xs font-thin whitespace-nowrap">
+              NATIONAL SOCIETY OF BLACK ENGINEERS
+            </div>
           </div>
-          <div className="text-nsbe-yellow-100 text-xs font-thin whitespace-nowrap">
-            NATIONAL SOCIETY OF BLACK ENGINEERS
+          <div className="mt-7">
+            {navigations.map(({ name, link }) => (
+              <Link key={name} href={link} className={getLinkStyle(link)}>
+                {name}
+              </Link>
+            ))}
           </div>
-        </div>
-        <div className="mt-7 ml-150">
-          <Link href="/home" className={getLinkStyle("/home")}>
-            HOME
-          </Link>
-          <Link href="/about" className={getLinkStyle("/about")}>
-            ABOUT
-          </Link>
-          <Link href="/board" className={getLinkStyle("/board")}>
-            BOARD
-          </Link>
-          <Link href="/events" className={getLinkStyle("/events")}>
-            EVENTS
-          </Link>
         </div>
       </div>
-      <div className="bg-nsbe-yellow-100 flex h-1 w-full flex-col"></div>
     </div>
   );
 };
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,6 +1,12 @@
-export const items = [
+export interface navigation {
+  name: string;
+  link: string;
+}
+const navigations: navigation[] = [
   { name: "HOME", link: "/home" },
   { name: "ABOUT", link: "/about" },
   { name: "BOARD", link: "/board" },
   { name: "EVENTS", link: "/events" },
 ];
+
+export default navigations;
