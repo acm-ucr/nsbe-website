668bd793ab0b694cfe5f87c3d29bc4771a49ebea
ManuChint <manu.chintalapathi@gmail.com>
2026-04-27 13:38:36 -0700

    applied changes to 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,27 +6,23 @@ import gearImg from "@/public/error/Gear.webp";
 
 const NotFound = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center select-none">
+    <div className="flex flex-col items-center justify-center">
       <div className="relative flex items-center justify-center">
-        <Image
-          src={gearImg}
-          alt="gear"
-          width={650}
-          height={650}
-          className="object-contain"
-        />
+        <Image src={gearImg} alt="gear" className="object-contain" />
 
-        <div className="absolute mt-10 mr-8 flex flex-col items-center text-center">
-          <p className="text-nsbe-yellow-100 text-9xl font-extrabold">404</p>
-          <p className="text-nsbe-yellow-100 text-4xl font-bold">
+        <div className="absolute flex flex-col items-center text-center">
+          <div className="text-nsbe-yellow-100 text-8xl font-extrabold">
+            404
+          </div>
+          <div className="text-nsbe-yellow-100 text-3xl font-bold">
             Page not found.
-          </p>
+          </div>
         </div>
       </div>
 
       <Link
         href="/"
-        className="border-nsbe-yellow-100 text-nsbe-yellow-100 mt-16 mr-16 rounded-xl border-3 px-5 py-4 text-2xl transition-opacity hover:opacity-60"
+        className="border-nsbe-yellow-100 text-nsbe-yellow-100 mr-10 rounded-xl border-3 px-5 py-3 text-xl transition-opacity hover:opacity-60"
       >
         RETURN HOME
       </Link>
