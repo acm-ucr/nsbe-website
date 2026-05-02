a8e3ac5951842c5cb130d21eb1da7784d3a89811
Mostafa Elmallah <mmmelmallah@gmail.com>
2026-05-01 21:25:55 -0700

    fixed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
+
 interface ButtonProps {
   text: string;
+  href: string;
 }
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text, href }: ButtonProps) => {
   return (
-    <button className="rounded-lg border-2 border-yellow-400 px-6 py-3 font-semibold tracking-wide text-yellow-400 uppercase">
-      <div> {text} </div>
-    </button>
+    <Link href={href}>
+      <button className="text-nsbe-yellow-400 rounded-lg border-2 border-yellow-400 px-6 py-3 font-semibold tracking-wide uppercase">
+        <div> {text} </div>
+      </button>
+    </Link>
   );
 };
 
