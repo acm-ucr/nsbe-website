0387c1b7a0e1c1b1edc58ce7247374c1102cac99
Alaya Billinger <Alaya.sanai@icloud.com>
2026-05-05 17:42:20 -0700

    made the about landing section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import OurMission from "@/components/about/OurMission";
 import History from "@/components/about/History";
 import JoinUs from "@/components/about/JoinUs";
+import Landing from "@/components/about/Landing";
 const About = () => {
   return (
     <>
+      <Landing />
       <OurMission />
       <History />
       <JoinUs />
--- src/components/about/Landing.tsx
+++ src/components/about/Landing.tsx
@@ -1,5 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import aboutLanding from "@/public/about/AboutLanding.webp";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="border-b-nsbe-yellow-100 relative flex h-[80vh] items-center justify-center overflow-hidden border-b-4">
+      <div className="absolute inset-0">
+        <Image
+          src={aboutLanding}
+          alt="NSBE UCR Group"
+          className="h-full w-full object-cover brightness-50"
+        />
+      </div>
+      <div className="text-nsbe-yellow-100 relative z-10 px-4 text-center">
+        <div className="text-nsbe-yellow-100 z-10 text-6xl font-extrabold">
+          ABOUT US
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
