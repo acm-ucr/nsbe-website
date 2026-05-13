5c7f6893dc66efd18de1e7388c2f454271bb587c
AYYYYYYJ <ajedwards881@gmail.com>
2026-05-12 23:34:01 -0700

    Formatted with eslint

--- src/components/home/Opportunities.tsx
+++ src/components/home/Opportunities.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Opportunities = () => {
   return (
-    <div className="py-10 px-6 pb-30">
-      <div className="max-w-6xl mx-auto">
-        <p className="text-nsbe-yellow-100 text-4xl font-bold text-right">
+    <div className="px-6 py-10 pb-30">
+      <div className="mx-auto max-w-6xl">
+        <p className="text-nsbe-yellow-100 text-right text-4xl font-bold">
           OPPORTUNITIES
         </p>
 
-        <div className="grid grid-cols-2 gap-24 items-start">
-      
+        <div className="grid grid-cols-2 items-start gap-24">
           <div className="relative">
-            <div className="absolute inset-0 bg-nsbe-red-100 rounded-2xl translate-x-4 translate-y-4" />
-            
-            <div className="relative bg-nsbe-gray-50 border p-10 rounded-2xl">
-              <p className="text-white text-3xl font-extrabold mb-6">
+            <div className="bg-nsbe-red-100 absolute inset-0 translate-x-4 translate-y-4 rounded-2xl" />
+
+            <div className="bg-nsbe-gray-50 relative rounded-2xl border p-10">
+              <p className="mb-6 text-3xl font-extrabold text-white">
                 NSBE FALL REGIONAL CONFERENCE
               </p>
-              <p className="text-gray-400 text-lg mb-10">
-                Excited to dip your toes in professional development and have the chance to meet students and recruiters from schools across the southwest? The Region VI Fall Conference is for you!
+              <p className="mb-10 text-lg text-gray-400">
+                Excited to dip your toes in professional development and have
+                the chance to meet students and recruiters from schools across
+                the southwest? The Region VI Fall Conference is for you!
               </p>
               <div className="self-start">
-                <a href="#" className="inline-block border-2 border-white text-white px-8 py-2 rounded-lg font-bold hover:bg-white hover:text-black">
+                <a
+                  href="#"
+                  className="inline-block rounded-lg border-2 border-white px-8 py-2 font-bold text-white hover:bg-white hover:text-black"
+                >
                   LEARN MORE
                 </a>
               </div>
@@ -29,23 +33,29 @@ const Opportunities = () => {
           </div>
 
           <div className="relative mt-32">
-            <div className="absolute inset-0 bg-nsbe-green-100 rounded-2xl translate-x-4 translate-y-4" />
-            
-            <div className="relative bg-nsbe-gray-50 border p-10 rounded-2xl flex flex-col">
-              <p className="text-white text-3xl font-extrabold mb-6">
+            <div className="bg-nsbe-green-100 absolute inset-0 translate-x-4 translate-y-4 rounded-2xl" />
+
+            <div className="bg-nsbe-gray-50 relative flex flex-col rounded-2xl border p-10">
+              <p className="mb-6 text-3xl font-extrabold text-white">
                 NSBE ANNUAL CONVENTION
               </p>
-              <p className="text-gray-400 text-lg mb-10">
-                Ready for a larger career fair? NSBE's annual convention exposes students to hundreds of companies hiring both interns and full-time new grads; there are limitless opportunities to connect with ambitious students and professionals in various engineering and financial fields!
+              <p className="mb-10 text-lg text-gray-400">
+                Ready for a larger career fair? NSBE's annual convention exposes
+                students to hundreds of companies hiring both interns and
+                full-time new grads; there are limitless opportunities to
+                connect with ambitious students and professionals in various
+                engineering and financial fields!
               </p>
               <div className="self-end">
-                <a href="#" className="inline-block border-2 border-white text-white px-8 py-2 rounded-lg font-extrabold hover:bg-white hover:text-black">
+                <a
+                  href="#"
+                  className="inline-block rounded-lg border-2 border-white px-8 py-2 font-extrabold text-white hover:bg-white hover:text-black"
+                >
                   LEARN MORE
                 </a>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
