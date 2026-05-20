96ad39045d38f3f4152dc7adc05479e6f5fc9b81
AYYYYYYJ <ajedwards881@gmail.com>
2026-05-20 15:11:29 -0700

    Made the screen look different on mobile versions than tablet/laptop

--- src/components/home/Opportunities.tsx
+++ src/components/home/Opportunities.tsx
@@ -2,21 +2,20 @@ import Link from "next/link";
 
 const Opportunities = () => {
   return (
-    <div className="px-6 py-10 pb-30">
+    <div className="px-6 py-10 pb-20 md:pb-30">
       <div className="mx-auto max-w-6xl">
-        <p className="text-nsbe-yellow-100 text-right text-4xl font-bold">
+        <p className="text-nsbe-yellow-100 mb-10 text-center text-3xl font-bold md:mb-0 md:text-right md:text-4xl">
           OPPORTUNITIES
         </p>
 
-        <div className="grid grid-cols-2 items-start gap-24">
+        <div className="grid grid-cols-1 items-start gap-12 md:grid-cols-2 md:gap-24">
           <div className="relative">
-            <div className="bg-nsbe-red-100 absolute inset-0 translate-x-4 translate-y-4 rounded-2xl" />
-
-            <div className="bg-nsbe-gray-50 relative rounded-2xl border p-10">
-              <p className="mb-6 text-3xl font-extrabold text-white">
+            <div className="bg-nsbe-red-100 absolute inset-0 translate-x-3 translate-y-3 rounded-2xl md:translate-x-4 md:translate-y-4" />
+            <div className="bg-nsbe-gray-50 relative rounded-2xl border p-6 md:p-10">
+              <p className="mb-4 text-2xl font-extrabold text-white md:mb-6 md:text-3xl">
                 NSBE FALL REGIONAL CONFERENCE
               </p>
-              <p className="mb-10 text-lg text-gray-400">
+              <p className="mb-8 text-base text-gray-400 md:mb-10 md:text-lg">
                 Excited to dip your toes in professional development and have
                 the chance to meet students and recruiters from schools across
                 the southwest? The Region VI Fall Conference is for you!
@@ -24,32 +23,31 @@ const Opportunities = () => {
               <div className="self-start">
                 <Link
                   href="#"
-                  className="inline-block rounded-lg border-2 border-white px-8 py-2 font-bold text-white hover:bg-white hover:text-black"
+                  className="inline-block rounded-lg border-2 border-white px-6 py-2 font-bold text-white transition-colors hover:bg-white hover:text-black md:px-8"
                 >
                   LEARN MORE
                 </Link>
               </div>
             </div>
           </div>
+          <div className="relative mt-4 md:mt-32">
+            <div className="bg-nsbe-green-100 absolute inset-0 translate-x-3 translate-y-3 rounded-2xl md:translate-x-4 md:translate-y-4" />
 
-          <div className="relative mt-32">
-            <div className="bg-nsbe-green-100 absolute inset-0 translate-x-4 translate-y-4 rounded-2xl" />
-
-            <div className="bg-nsbe-gray-50 relative flex flex-col rounded-2xl border p-10">
-              <p className="mb-6 text-3xl font-extrabold text-white">
+            <div className="bg-nsbe-gray-50 relative flex flex-col rounded-2xl border p-6 md:p-10">
+              <p className="mb-4 text-2xl font-extrabold text-white md:mb-6 md:text-3xl">
                 NSBE ANNUAL CONVENTION
               </p>
-              <p className="mb-10 text-lg text-gray-400">
+              <p className="mb-8 text-base text-gray-400 md:mb-10 md:text-lg">
                 Ready for a larger career fair? NSBE's annual convention exposes
                 students to hundreds of companies hiring both interns and
                 full-time new grads; there are limitless opportunities to
                 connect with ambitious students and professionals in various
                 engineering and financial fields!
               </p>
-              <div className="self-end">
+              <div className="self-start md:self-end">
                 <Link
                   href="#"
-                  className="inline-block rounded-lg border-2 border-white px-8 py-2 font-extrabold text-white hover:bg-white hover:text-black"
+                  className="inline-block rounded-lg border-2 border-white px-6 py-2 font-extrabold text-white transition-colors hover:bg-white hover:text-black md:px-8"
                 >
                   LEARN MORE
                 </Link>
