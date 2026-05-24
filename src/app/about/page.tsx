6c69c83f708e5ac56a3802b954f8cace995dc6b7
Alaya Billinger <Alaya.sanai@icloud.com>
2026-05-24 00:58:18 -0700

    made the about gallery strip

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,13 @@ import OurMission from "@/components/about/OurMission";
 import History from "@/components/about/History";
 import JoinUs from "@/components/about/JoinUs";
 import Landing from "@/components/about/Landing";
+import Gallery from "@/components/Gallery";
+import aboutGallery from "@/data/aboutGallery";
 const About = () => {
   return (
     <>
       <Landing />
+      <Gallery images={aboutGallery} />
       <OurMission />
       <History />
       <JoinUs />
---
+++ src/data/aboutGallery.ts
@@ -0,0 +1,25 @@
+import gallery1 from "@/public/about/AboutGallery1.webp";
+import gallery2 from "@/public/about/AboutGallery2.webp";
+import gallery3 from "@/public/about/AboutGallery3.webp";
+import gallery4 from "@/public/about/AboutGallery4.webp";
+
+export const gallery = [
+  {
+    image: gallery1,
+    alt: "Gallery Image 1",
+  },
+  {
+    image: gallery2,
+    alt: "Gallery Image 2",
+  },
+  {
+    image: gallery3,
+    alt: "Gallery Image 3",
+  },
+  {
+    image: gallery4,
+    alt: "Gallery Image 4",
+  },
+];
+
+export default gallery;
